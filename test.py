fd825f8826b2b6476fd5fb685c9f6dfe7b96fa68
los16dura <Ulechka_1999@mail.ru>
2016-09-30 23:39:43 +0300

    +3 Task

--- test.py
+++ test.py
@@ -1,48 +1,71 @@
-#1
-A = []
-for i in range(int(input())):
-    A.append(int(input()))
+# #1
+# A = []
+# for i in range(int(input())):
+#     A.append(int(input()))
+# # for i in range(len(A)):
+# #1-1
+# print(A[1::2])
+# #1-2
+# print(max(A))
+# #1-3
+# print(A[::-1])
+# i=0
+#
+# #2
+#
+#
+# #2-1
+# while i < (len(A)-1):
+#     A[i], A[i + 1] = A[i + 1], A[i]
+#     i+=2
+# print(A)
+#
+#
+# #2-2
+#
+# i=0
+# for i in range(len(A)-1):
+#      #a=A[0]
+#      a = A.pop(0)
+#      A.append(a)
+# print(A)
+#
+# #2-3
+# i= 0
 # for i in range(len(A)):
-#1-1
-print(A[1::2])
-#1-2
-print(max(A))
-#1-3
-print(A[::-1])
-i=0
-
-#2
-
-
-#2-1
-while i < (len(A)-1):
-    A[i], A[i + 1] = A[i + 1], A[i]
-    i+=2
-print(A)
+#  if  A.count(A[i])==1:
+#      print(A[i],end=' ')
+#
+# print()
+#
+A = [1, 3, 2, 32,32]
+# #2-4
+# max=1
+# max_i=1
+# for i in range(len(A)):
+#     if A.count(A[i])> max:
+#         max=A.count(A[i])
+#         max_i=i
+# print(A[max_i])
 
 
-#2-2
-
-i=0
-for i in range(len(A)-1):
-     #a=A[0]
-     a = A.pop(0)
-     A.append(a)
-print(A)
-
-#2-3
-i= 0
+#3
+k = 0
 for i in range(len(A)):
- if  A.count(A[i])==1:
-     print(A[i],end=' ')
+    if k >= len(A):
+        break
+    #print('k=',k)
+    print(A[i],end=' ')
+    k=k+1
+    if k >= len(A):
+        break
 
-print()
+    print(A[len(A)-i-1],end=' ')
+    k = k + 1
 
-#2-4
-max=1
-max_i=1
-for i in range(len(A)):
-    if A.count(A[i])> max:
-        max=A.count(A[i])
-        max_i=i
-print(A[max_i])
+
+
+#4
+
+
+
