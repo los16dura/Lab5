bbca610a535396a1f278c545affd4063bd64ee0b
los16dura <Ulechka_1999@mail.ru>
2016-09-30 15:08:41 +0300

    Task 1-2

--- test.py
+++ test.py
@@ -1,10 +1,48 @@
+#1
 A = []
 for i in range(int(input())):
     A.append(int(input()))
 # for i in range(len(A)):
-print(A[::2])
+#1-1
+print(A[1::2])
+#1-2
 print(max(A))
+#1-3
 print(A[::-1])
-for i in range(len(A)):
-    a[i]=a[i+1]
+i=0
+
+#2
+
+
+#2-1
+while i < (len(A)-1):
+    A[i], A[i + 1] = A[i + 1], A[i]
     i+=2
+print(A)
+
+
+#2-2
+
+i=0
+for i in range(len(A)-1):
+     #a=A[0]
+     a = A.pop(0)
+     A.append(a)
+print(A)
+
+#2-3
+i= 0
+for i in range(len(A)):
+ if  A.count(A[i])==1:
+     print(A[i],end=' ')
+
+print()
+
+#2-4
+max=1
+max_i=1
+for i in range(len(A)):
+    if A.count(A[i])> max:
+        max=A.count(A[i])
+        max_i=i
+print(A[max_i])
