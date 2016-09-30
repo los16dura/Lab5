d32fef0cba5f314fd1c971a1d38c205132e561dc
los16dura <Ulechka_1999@mail.ru>
2016-09-30 13:47:54 +0300

    Probnoe

---
+++ test.py
@@ -0,0 +1,10 @@
+A = []
+for i in range(int(input())):
+    A.append(int(input()))
+# for i in range(len(A)):
+print(A[::2])
+print(max(A))
+print(A[::-1])
+for i in range(len(A)):
+    a[i]=a[i+1]
+    i+=2
